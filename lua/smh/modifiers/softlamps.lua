f87bd2d80da66ae825c8fe3e09691005e3a1764c
vlazed <35244845+vlazed@users.noreply.github.com>
2026-06-17 14:05:13 -0400

    Implement SENT modifier - Reroute some old modifiers to use the universal SENT modifier, for backwards compatibility

--- lua/smh/modifiers/softlamps.lua
+++ lua/smh/modifiers/softlamps.lua
@@ -1,5 +1,6 @@
-
-MOD.Name = "Soft Lamps";
+-- Backwards compat
+include("sent.lua")
+MOD.Name = "Soft Lamps (deprecated)";
 
 function MOD:IsSoftLamp(entity)
 
@@ -8,37 +9,18 @@ function MOD:IsSoftLamp(entity)
 
 end
 
-function MOD:Save(entity)
-
-    if not self:IsSoftLamp(entity) then return nil; end
-
-    local data = {};
-
-    data.FOV = entity:GetLightFOV();
-    data.Nearz = entity:GetNearZ();
-    data.Farz = entity:GetFarZ();
-    data.Brightness = entity:GetBrightness();
-    data.Color = entity:GetLightColor();
-    data.ShapeRadius = entity:GetShapeRadius();
-    data.FocalPoint = entity:GetFocalDistance();
-    data.Offset = entity:GetLightOffset();
-
-    return data;
-
-end
-
 function MOD:Load(entity, data)
 
     if not self:IsSoftLamp(entity) then return; end -- can never be too sure?
 
-    entity:SetLightFOV(data.FOV);
-    entity:SetNearZ(data.Nearz);
-    entity:SetFarZ(data.Farz);
+    entity:SetLightFOV(data.FOV or data.LightFOV);
+    entity:SetNearZ(data.Nearz or data.NearZ);
+    entity:SetFarZ(data.Farz or data.FarZ);
     entity:SetBrightness(data.Brightness);
-    entity:SetLightColor(data.Color);
-    entity:SetShapeRadius(data.ShapeRadius);
-    entity:SetFocalDistance(data.FocalPoint);
-    entity:SetLightOffset(data.Offset);
+    entity:SetLightColor(data.Color or data.LightColor);
+    entity:SetShapeRadius(data.ShapeRadius or data.ShapeRadius);
+    entity:SetFocalDistance(data.FocalPoint or data.FocalDistance);
+    entity:SetLightOffset(data.Offset or data.LightOffset);
 
 end
 
@@ -46,13 +28,13 @@ function MOD:LoadBetween(entity, data1, data2, percentage)
 
     if not self:IsSoftLamp(entity) then return; end -- can never be too sure?
 
-    entity:SetLightFOV(SMH.LerpLinear(data1.FOV, data2.FOV, percentage));
-    entity:SetNearZ(SMH.LerpLinear(data1.Nearz, data2.Nearz, percentage));
-    entity:SetFarZ(SMH.LerpLinear(data1.Farz, data2.Farz, percentage));
+    entity:SetLightFOV(SMH.LerpLinear(data1.FOV or data1.LightFOV, data2.FOV or data2.LightFOV, percentage));
+    entity:SetNearZ(SMH.LerpLinear(data1.Nearz or data1.NearZ, data2.Nearz or data2.NearZ, percentage));
+    entity:SetFarZ(SMH.LerpLinear(data1.Farz or data1.FarZ, data2.Farz or data2.FarZ, percentage));
     entity:SetBrightness(SMH.LerpLinear(data1.Brightness, data2.Brightness, percentage));
-    entity:SetLightColor(SMH.LerpLinearVector(data1.Color, data2.Color, percentage));
+    entity:SetLightColor(SMH.LerpLinearVector(data1.Color or data1.LightColor, data2.Color or data2.LightColor, percentage));
     entity:SetShapeRadius(SMH.LerpLinear(data1.ShapeRadius, data2.ShapeRadius, percentage));
-    entity:SetFocalDistance(SMH.LerpLinear(data1.FocalPoint, data2.FocalPoint, percentage));
-    entity:SetLightOffset(SMH.LerpLinearVector(data1.Offset, data2.Offset, percentage));
+    entity:SetFocalDistance(SMH.LerpLinear(data1.FocalPoint or data1.FocalDistance, data2.FocalPoint or data2.FocalDistance, percentage));
+    entity:SetLightOffset(SMH.LerpLinearVector(data1.Offset or data1.LightOffset, data2.Offset or data2.LightOffset, percentage));
 
 end
